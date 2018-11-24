2a2b38c66bb298f8b838251667c7fca6059c152b
Храмцов Антон Ильич <Khramtsov.A.I@sberbank.ru>
2018-11-24 23:44:39 +0300

    Добавил flyWay и автоматическу проливку скриптов

--- src/main/java/com/accenture/SmartOffice/scripts/00006_add_users.sql
+++
@@ -1,9 +0,0 @@
-insert into users values(1, 10, 'anton.khramtsov@accenture.com', 'Anton', '1', 'url', 'Khramtsov', 'a_khramtsov', 'Ilych', '+79106478953', 1);
-
-insert into users values(2, 6, 'john.doe@accenture.com', 'John', '1', 'url', 'doe', 'j_doe', null, '+78005553535', 2);
-
-insert into users values(3, 6, 'mike.sichalov@accenture.com', 'Mike', '1', 'url', 'Sichalov', 'm_sichalov', 'Michailovich', '+78005553535', 1);
-
-insert into users values(4, 6, 'alexander.lebedev@accenture.com', 'Alexander', '1', 'url', 'Lebedev', 'a_lebedev', 'Vasilevic', '+78005553535', 1);
-
-insert into users values(5, 6, 'alexander.letvinenko@accenture.com', 'Alexander', '1', 'url', 'Letvinenko', 'a_letvinenko', 'Yurievich', '+78005553535', 1);
