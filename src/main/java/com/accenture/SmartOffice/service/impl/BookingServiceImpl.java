3387788bb2c73dca8491b2ad318bf7bb0c7548c0
Храмцов Антон Ильич <Khramtsov.A.I@sberbank.ru>
2018-11-24 19:35:49 +0300

    fix BookingService

--- src/main/java/com/accenture/SmartOffice/service/impl/BookingServiceImpl.java
+++ src/main/java/com/accenture/SmartOffice/service/impl/BookingServiceImpl.java
@@ -1,6 +1,8 @@
 package com.accenture.SmartOffice.service.impl;
 
 import com.accenture.SmartOffice.service.BookingService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookingServiceImpl implements BookingService {
 }
